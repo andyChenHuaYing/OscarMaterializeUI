37e6087da28390cb814a78c15060d5dc19842360
461857202 <461857202@qq.com>
2016-12-27 21:15:24 +0800

    Finished Login.html

---
+++ static/js/Login.js
@@ -0,0 +1,9 @@
+/**
+    * Description :
+    *   Login html init js.
+    * @author : oscar
+    * @version   :1.0, 2016/12/27
+    */
+$(document).ready(function () {
+    $('.login-content').css('marginTop', ($(window).height() - $('.login-content > div').height()) / 2);
+});
