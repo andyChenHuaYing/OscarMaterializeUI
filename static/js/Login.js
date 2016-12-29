4351feb106ca86447c93de3a5b4fa0b0ca8e095d
461857202 <461857202@qq.com>
2016-12-29 11:20:52 +0800

    Finish message dialog.

--- static/js/Login.js
+++ static/js/Login.js
@@ -1,10 +1,35 @@
 /**
-    * Description :
-    *   Login html init js.
-    * @author : oscar
-    * @version   :1.0, 2016/12/27
-    */
-$(document).ready(function () {
-    $('.login-content').css('marginTop', ($(window).height() - $('.login-content > div').height()) / 2);
-});
+ * Description :
+ *   Login html init js.
+ * @author : oscar
+ * @version   :1.0, 2016/12/27
+ */
+(function ($) {
+    function LoginFun() {}
 
+    $(document).ready(function () {
+        $('.login-content').css('marginTop', ($(window).height() - $('.login-content > div').height()) / 2);
+    });
+
+    /**
+     * Verify username and password. This method aim to demonstrate how to use system confirm modal function.
+     * Default : oscar/oscar
+     */
+    LoginFun.prototype.checkInput = function () {
+        if ($('#username').val().length == 0) {
+            $('#username').focus();
+            $('#username').addClass('invalid');
+            oscarUI.messageDialog(oscarUI.messageType.warning, 'Username must not null!');
+            return false;
+        }
+
+        oscarUI.confirm('Login system with default account ?', 'Index.html');
+        return false;
+    };
+
+    if(!window.loginFun) {
+        window.loginFun = new LoginFun();
+    }else {
+        alert('Global variable name "loginFun" already exist, please use another one!');
+    }
+})(jQuery);
--- static/js/oscar-materialize-css-ui.js
+++ static/js/oscar-materialize-css-ui.js
@@ -7,6 +7,14 @@
 (function ($) {
     function OscarUI() {
         this.sideBarHtml = $('.button-collapse');
+
+        // Message type
+        this.messageType = {
+            "success": 0,
+            "tips": 1,
+            "warning": 2,
+            "error": 3
+        }
     }
 
     /**
@@ -36,68 +44,112 @@
     };
     /**
      * System confirm modal.
-     * @param tips
+     * @param tips  Alert content
+     * @param yesCallback Confirm callback function or url
+     * @param noCallback Cancel callback function
+     * @param title Confirm modal title
      */
-    OscarUI.prototype.confirm = function (tips, callback) {
-
+    OscarUI.prototype.confirm = function (tips, yesCallback, noCallback, title) {
         if ($('#systemConfirmModal').length == 0) {
             var html =
-                '<div id="systemConfirmModal" class="ctm-confirm-modal modal modal-fixed-footer "> ' +
-                    '<div class="modal-content">' +
-                        '<h5>System Alert</h5> ' +
-                        '<hr/>' +
-                        '<p class="ctm-confirm-modal-content">' + tips + '</p> ' +
-                    '</div>' +
-                    '<div class="modal-footer">' +
-                        '<a id="confirmNo" class="ctm-confirm-cancel modal-action modal-close waves-effect blue-grey  btn">' +
-                            '<i class="mdi-content-clear"></i>' +
-                        '</a>' +
-                        '<a id="confirmYes" class="modal-action modal-close waves-effect waves-green offset-s1 btn ">' +
-                            '<i class="mdi-action-done"></i>' +
-                        '</a>' +
-                    '</div>' +
+                '<div id="systemConfirmModal" class="ctm-confirm-modal modal modal-fixed-footer ">' +
+                '<div class="modal-content">' +
+                '<h5>' + (title ? title : "System Alert") + '</h5> ' +
+                '<hr/>' +
+                '<p class="ctm-confirm-modal-content">' + tips + '</p> ' +
+                '</div>' +
+                '<div class="modal-footer">' +
+                '<a id="confirmNo" class="ctm-confirm-cancel modal-action modal-close waves-effect blue-grey  btn">' +
+                '<i class="mdi-content-clear"></i>' +
+                '</a>' +
+                '<a id="confirmYes" class="modal-action modal-close waves-effect waves-green offset-s1 btn ">' +
+                '<i class="mdi-action-done"></i>' +
+                '</a>' +
+                '</div>' +
                 '</div>';
             $('body').append(html);
         }
-
 
         $('#systemConfirmModal').openModal({
             dismissible: true, // 点击模态框外部则关闭模态框
             opacity: .5, // 背景透明度
             in_duration: 300, // 切入时间
-            out_duration: 200 ,// 切出时间
+            out_duration: 200,// 切出时间
             ready: function () {
                 $('#confirmYes').on('click', function () {
-                    alert('Confirm Yes');
-                    if(typeof callback == 'function'){
-                        callback();
-                    } else if (typeof callback == 'string'){
-                        window.location.href = callback;
+                    if (typeof yesCallback == 'function') {
+                        yesCallback();
+                    } else if (typeof yesCallback == 'string') {
+                        window.location.href = yesCallback;
+                    }
+                });
+
+                $('#confirmNo').on('click', function () {
+                    if (typeof noCallback == 'function') {
+                        noCallback();
                     }
                 })
             },
-
             complete: function () {
-
             }
         });
-
-
-        debugger;
-        $('#confirmYes').click = function () {
-            alert('Confirm Yes');
-            callback();
-        }
     };
 
     /**
-     * Verify username and password. This method aim to demonstrate how to use system confirm modal function.
-     * Default : oscar/oscar
+     * Message dialog, support success, fail, error, tips type.
+     * @param messageType #see OscarUI.messageType
+     * @param message Content
+     * @param optionalTitleIcon Custom Message title icon. Note: only materialcss type icon work.
      */
-    OscarUI.prototype.checkInput = function () {
-        return this.confirm('Login system with default account ?', 'Index.html');
-    };
+    OscarUI.prototype.messageDialog = function (messageType, message, optionalTitleIcon) {
+        var messageDialogIcon;
+        if (optionalTitleIcon) {
+            messageDialogIcon = optionalTitleIcon;
+        } else {
+            switch (messageType) {
+                case 0:
+                    messageDialogIcon = "mdi-action-done";
+                    break;
+                case 1:
+                    messageDialogIcon = "mdi-action-done";
+                    break;
+                case 2:
+                    messageDialogIcon = "mdi-alert-warning";
+                    break;
+                case 3:
+                    messageDialogIcon = "mdi-alert-error";
+                    break;
+            }
+        }
+        // messageDialog
 
+        $('#messageDialog').remove();
+
+        var messageDialogHtml =
+            '<div id="messageDialog" class="ctm-confirm-modal modal modal-fixed-footer "> ' +
+                '<div class="modal-content"> ' +
+                    '<h5 ><i class="' + messageDialogIcon + '"></i>&nbsp;&nbsp;System Alert </h5> ' +
+                    '<hr/> ' +
+                    '<p class="ctm-confirm-modal-content">' + message + '</p> ' +
+                '</div> ' +
+                '<div class="modal-footer center">' +
+                    '<a class="modal-action modal-close waves-effect waves-green offset-s1 btn ">' +
+                        'OK' +
+                    '</a>' +
+                '</div>' +
+            '</div>';
+
+        $('body').append(messageDialogHtml);
+
+        $('#messageDialog').openModal({
+            dismissible: true, // 点击模态框外部则关闭模态框
+            opacity: .5, // 背景透明度
+            in_duration: 300, // 切入时间
+            out_duration: 200,// 切出时间
+        });
+
+
+    };
 
     /**
      * Inject global variable oscarUI, oscarConflictUI will be used if oscarUI has existed.
